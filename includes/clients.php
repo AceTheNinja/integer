149a54b1168fd53675b400c8b1aa3e4fc298847e
Tapan Rai <raitapan1@gmail.com>
2017-05-17 20:37:21 +0530

    changes

--- includes/clients.php
+++ includes/clients.php
@@ -27,10 +27,10 @@
                         </div>
                         <div class="col-lg-3" style="float: right;">
                             <img src="../images/clients/college13.jpg" alt="dell">
-                        </div><div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/college8.jpg" alt="dell">
                         </div>
                         <div class="col-lg-3" style="float: right;">
+                            <img src="../images/clients/college2.jpg" alt="TCS">
+                        </div><div class="col-lg-3" style="float: right;">
                             <img src="../images/clients/college10.jpg" alt="dell">
                         </div>
                         <div class="col-lg-3" style="float: right;">
@@ -42,9 +42,11 @@
                         <div class="col-lg-3" style="float: right;">
                             <img src="../images/clients/college14.jpg" alt="dell">
                         </div>
+                        
                         <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/college2.jpg" alt="TCS">
+                            <img src="../images/clients/college8.jpg" alt="dell">
                         </div>
+                </div>
             </div>
         </div>
 </section>
@@ -58,33 +60,33 @@
                         <div class="row">
                         <h1 class="title center-align othercourse">Corporate Clients</h1>
                         <br>
-                        <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/client1.jpg" alt="aricent">
-                        </div>
-                        <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/client2.jpg" alt="TCS">
-                        </div>
-                        <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/client3.jpg" alt="capgemini">
-                        </div>
-                        <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/client4.jpg" alt="hp">
-                        </div>                   
-                        <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/client5.jpg" alt="tech">
-                        </div>
-                        <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/client6.jpg" alt="infosys">
-                        </div>
-                        <div class="col-lg-3" style="float: right;">
+                        
+                        <div class="col-lg-3">
                             <img src="../images/clients/client7.jpg" alt="wipro">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3">
+                            <img src="../images/clients/client1.jpg" alt="aricent">
+                        </div><div class="col-lg-3">
+                            <img src="../images/clients/client9.jpg" alt="dell">
+                        </div><div class="col-lg-3">
+                            <img src="../images/clients/client5.jpg" alt="tech">
+                        </div><div class="col-lg-3">
+                            <img src="../images/clients/client3.jpg" alt="capgemini">
+                        </div>
+                        <div class="col-lg-3">
+                            <img src="../images/clients/client4.jpg" alt="hp">
+                        </div>                   
+                        
+                        <div class="col-lg-3">
+                            <img src="../images/clients/client2.jpg" alt="TCS">
+                        </div><div class="col-lg-3">
+                            <img src="../images/clients/client6.jpg" alt="infosys">
+                        </div>
+                        
+                        <div class="col-lg-3">
                             <img src="../images/clients/client8.jpg" alt="dell">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
-                            <img src="../images/clients/client9.jpg" alt="dell">
-                        </div>
+                        
                     </div>
                         
                     </div>   

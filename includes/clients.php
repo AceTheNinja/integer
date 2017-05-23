93abdca4d04826c42b7ebf178964c514402e2d1d
Tapan Rai <raitapan1@gmail.com>
2017-05-23 19:00:28 +0530

    changes

--- includes/clients.php
+++ includes/clients.php
@@ -5,45 +5,45 @@
                 <div class="row">
                         <h1 class="title center-align othercourse">College Clients</h1>
                         <br>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college1.jpg" alt="aricent">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college3.jpg" alt="capgemini">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college4.jpg" alt="hp">
                         </div>                   
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college5.jpg" alt="tech">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college9.jpg" alt="dell">
-                        </div><div class="col-lg-3" style="float: right;">
+                        </div><div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college6.jpg" alt="infosys">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college7.jpg" alt="wipro">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college13.jpg" alt="dell">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college2.jpg" alt="TCS">
-                        </div><div class="col-lg-3" style="float: right;">
+                        </div><div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college10.jpg" alt="dell">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college11.jpg" alt="dell">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college12.jpg" alt="dell">
                         </div>
-                        <div class="col-lg-3" style="float: right;">
+                        <div class="col-lg-3 center-align" style="float: right;">
                             <img src="../images/clients/college14.jpg" alt="dell">
                         </div>
                         
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 center-align">
                             <img src="../images/clients/college8.jpg" alt="dell">
                         </div>
                 </div>
@@ -61,29 +61,29 @@
                         <h1 class="title center-align othercourse">Corporate Clients</h1>
                         <br>
                         
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 center-align">
                             <img src="../images/clients/client7.jpg" alt="wipro">
                         </div>
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 center-align">
                             <img src="../images/clients/client1.jpg" alt="aricent">
-                        </div><div class="col-lg-3">
+                        </div><div class="col-lg-3 center-align">
                             <img src="../images/clients/client9.jpg" alt="dell">
-                        </div><div class="col-lg-3">
+                        </div><div class="col-lg-3 center-align">
                             <img src="../images/clients/client5.jpg" alt="tech">
-                        </div><div class="col-lg-3">
+                        </div><div class="col-lg-3 center-align">
                             <img src="../images/clients/client3.jpg" alt="capgemini">
                         </div>
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 center-align">
                             <img src="../images/clients/client4.jpg" alt="hp">
                         </div>                   
                         
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 center-align">
                             <img src="../images/clients/client2.jpg" alt="TCS">
-                        </div><div class="col-lg-3">
+                        </div><div class="col-lg-3 center-align">
                             <img src="../images/clients/client6.jpg" alt="infosys">
                         </div>
                         
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 center-align">
                             <img src="../images/clients/client8.jpg" alt="dell">
                         </div>
                         
